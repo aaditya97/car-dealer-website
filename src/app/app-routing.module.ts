a70cb3696ecbe265ffdb41ea6fded5d51e08ed21
sagarbhagat10 <sagarbhagat10@gmail.com>
2018-04-03 17:33:52 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CompanyComponent} from './company/company.component';
+import {CarComponent} from './car/car.component';
+import {ModelComponent} from './model/model.component';
+import {PriceComponent} from './price/price.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'company',
+    component: CompanyComponent
+  },
+  {
+    path: 'car',
+    component: CarComponent
+  },
+  {
+    path: 'model',
+    component: ModelComponent
+  },
+  {
+    path: 'price',
+    component: PriceComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
